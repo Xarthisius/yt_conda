9d62013788d14011eb875cc85d56fb3b133e965a
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-24 15:06:52 +0200

    [hdf5] update

--- hdf5/build.sh
+++ hdf5/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --disable-static \
-    --enable-linux-lfs --with-zlib=$PREFIX --enable-shared \
+./configure --prefix="${PREFIX}" --disable-static \
+    --enable-linux-lfs --with-zlib="${PREFIX}" --enable-shared \
     --enable-production --disable-silent-rules \
-    --disable-parallel
+    --disable-parallel \
+    --disable-fortran --disable-fortran2003
 make -j9
 make install
 
-rm -rf $PREFIX/share/hdf5_examples
+rm -rf "${PREFIX}"/share/hdf5_examples
+find "${PREFIX}" -name "*.la" -delete
