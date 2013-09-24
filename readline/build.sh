e5d5c949103dc134b19e7b8f87c7652a514ba636
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-24 11:56:43 +0200

    add readline

---
+++ readline/build.sh
@@ -0,0 +1,11 @@
+sed -i -e 's:^SHLIB_LIBS=:SHLIB_LIBS=-lncurses:' support/shobj-conf
+
+
+CPPFLAGS="-D_GNU_SOURCE -Dxrealloc=_rl_realloc -Dxmalloc=_rl_malloc -Dxfree=_rl_free"
+
+./configure \
+	--cache-file="${S}"/config.cache \
+	--with-curses \
+	--prefix="$PREFIX"
+make -j4
+make install DESTDIR="$PREFIX"
