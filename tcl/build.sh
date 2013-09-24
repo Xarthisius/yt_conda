fc66acb4e310103c47cb5e39af00d8fdc68d37de
Kacper Kowalik (Xarthisius) <xarthisius@gentoo.org>
2013-09-24 18:00:00 +0200

    [tcl] fix

--- tcl/build.sh
+++ tcl/build.sh
@@ -34,7 +34,7 @@ cp "${SPARENT}"/generic/*.h "$PREFIX"/lib/tcl${v1}/include/generic
 rm -f "${PREFIX}"/lib/tcl${v1}/include/generic/{tcl,tclDecls,tclPlatDecls}.h
 
 # install symlink for libraries
-ln -s libtcl${v1}$(get_libname) "${PREFIX}"/lib/libtcl$(get_libname)
+ln -s libtcl${v1}.so "${PREFIX}"/lib/libtcl.so
 ln -s libtclstub${v1}.a "${PREFIX}"/lib/libtclstub.a
 
 ln -s tclsh${v1} "${PREFIX}"/bin/tclsh
