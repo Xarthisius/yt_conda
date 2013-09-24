76e8a91a155fe6f769ea7c9cf3e2619647bd7903
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-24 13:49:40 +0200

    [bzip2] Fix build

--- bzip2/build.sh
+++ bzip2/build.sh
@@ -2,3 +2,9 @@ make -f Makefile-libbz2_so all
 make all
 make install PREFIX="${PREFIX}"
 find "${PREFIX}" -name "lib*.a" -delete
+
+mkdir -p "$PREFIX"/lib
+mv libbz2.so.1.0.6 "$PREFIX"/lib
+ln -s libbz2.so.1.0.6 "$PREFIX"/lib/libbz2.so.1
+ln -s libbz2.so.1.0.6 "$PREFIX"/lib/libbz2.so
+mv bzip2-shared "${PREFIX}"/bin/bzip2
