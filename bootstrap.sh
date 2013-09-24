031620e6c59072f3fc161875b1f1a9c85ff77984
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-24 11:39:01 +0200

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+CONDABIN=Miniconda-1.9.1-Linux-x86_64.sh
+
+rm -rf yt-conda
+[[ ! -e ${CONDABIN} ]] && wget -nc http://repo.continuum.io/miniconda/${CONDABIN}
+bash ${CONDABIN} -b -p $PWD/yt-conda
+export PATH="$PWD/yt-conda/bin:$PATH"
+
+conda build --no-binstar-upload zlib
+conda install yt-conda/conda-bld/linux-64/zlib-*.tar.bz2
+conda build --no-binstar-upload bzip2
+conda install yt-conda/conda-bld/linux-64/bzip-*.tar.bz2
+conda build --no-binstar-upload ncurses
+conda install yt-conda/conda-bld/linux-64/ncurses-*.tar.bz2
+
+conda list
