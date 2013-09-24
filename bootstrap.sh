0573d6c8aa00e586207d13229e1a4de2d51bdfc8
Kacper Kowalik (Xarthisius) <xarthisius@gentoo.org>
2013-09-24 18:36:51 +0200

    [python] skip libffi for now, update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ rm -rf yt-conda
 bash ${CONDABIN} -b -p $PWD/yt-conda
 export PATH="$PWD/yt-conda/bin:$PATH"
 
-for pkg in zlib bzip2 ncurses openssl libffi readline sqlite hdf5; do
+for pkg in zlib bzip2 ncurses openssl readline sqlite tcl tk libpng hdf5 python; do
    conda build --no-binstar-upload ${pkg}
    conda install yt-conda/conda-bld/linux-64/${pkg}-*.tar.bz2
 done
--- python/build.sh
+++ python/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #rm -r Modules/expat
-rm -r Modules/_ctypes/libffi*
+#rm -r Modules/_ctypes/libffi*
 rm -r Modules/zlib
 
 ./configure \
