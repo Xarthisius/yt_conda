98d63349a1ac8fc972c551b6f55d1d2f73df87e4
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-24 14:24:56 +0200

    [zlib] update

--- zlib/build.sh
+++ zlib/build.sh
@@ -1,3 +1,6 @@
-./configure --prefix=$PREFIX --disable-static
+./configure --prefix=$PREFIX --shared
 make -j9
 make install
+
+sed -i -r 's:\<(O[FN])\>:_Z_\1:g' "${PREFIX}"/include/*.h
+find "${PREFIX}" -name "libz.*a" -delete
