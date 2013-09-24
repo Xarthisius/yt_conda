fc15b0bda38f9b78dbb7e75894e79cbd3253c9a9
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-24 14:08:28 +0200

    [libffi] disable static, fix paths

--- libffi/build.sh
+++ libffi/build.sh
@@ -1,3 +1,5 @@
-./configure --prefix="$PREFIX"
+sed -i 's:@toolexeclibdir@:$(libdir):g' Makefile.in
+./configure --prefix="$PREFIX" --disable-static --libdir="$PREFIX"/lib
 make -j4
-make install DESTDIR="$PREFIX"
+make install
+find "$PREFIX" -name "*.la" -delete
