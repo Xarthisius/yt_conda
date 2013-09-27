29cac7320c5236c25f175429905f0d1cd9712c0f
Kacper Kowalik (Xarthisius) <xarthisius@gentoo.org>
2013-09-27 18:25:27 +0200

    [bzip2] fix links

--- bzip2/build.sh
+++ bzip2/build.sh
@@ -8,3 +8,9 @@ mv libbz2.so.1.0.6 "$PREFIX"/lib
 ln -s libbz2.so.1.0.6 "$PREFIX"/lib/libbz2.so.1
 ln -s libbz2.so.1.0.6 "$PREFIX"/lib/libbz2.so
 mv bzip2-shared "${PREFIX}"/bin/bzip2
+
+rm "${PREFIX}"/bin/bz{cmp,egrep,fgrep,less}
+ln -s bzgrep "$PREFIX"/bin/bzegrep
+ln -s bzgrep "$PREFIX"/bin/bzfgrep
+ln -s bzdiff "$PREFIX"/bin/bzcmp
+ln -s bzmore "$PREFIX"/bin/bzless
