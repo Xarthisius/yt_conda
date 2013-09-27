ac364ca7c3baa6256d5730899125312e63cad3b9
Kacper Kowalik (Xarthisius) <xarthisius@gentoo.org>
2013-09-27 19:21:02 +0200

    add more packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,14 @@ rm -rf yt-conda
 bash ${CONDABIN} -b -p $PWD/yt-conda
 export PATH="$PWD/yt-conda/bin:$PATH"
 
-for pkg in zlib bzip2 ncurses openssl readline sqlite tcl tk libpng hdf5 blas lapack python cmake; do
+for pkg in zlib bzip2 ncurses openssl readline sqlite tcl tk libpng hdf5 blas lapack python cmake freetype; do
    conda build --no-binstar-upload ${pkg}
    conda install yt-conda/conda-bld/linux-64/${pkg}-*.tar.bz2
 done
 
 conda create -n py_yt python -c http://tinyurl.com/yt-conda --override-channels
 source activate py_yt
-for pkg in setuptools numpy cython mercurial; do
+for pkg in setuptools numpy cython mercurial h5py yt; do
    conda build --no-binstar-upload ${pkg}
    conda install yt-conda/conda-bld/linux-64/${pkg}-*.tar.bz2
 done
