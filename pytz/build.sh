a3422c8ff8526d1f0bd7281b264ae481fa581bea
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-09-28 09:42:47 +0200

    [pytz] fix build issue

--- pytz/build.sh
+++ pytz/build.sh
@@ -1,1 +1,14 @@
-$PYTHON setup.py install
+$PYTHON setup.py build \
+   --build-base "${SRC_DIR}"/build \
+   --build-platlib "${SRC_DIR}"/build/lib \
+   --build-purelib "${SRC_DIR}"/build/lib \
+   --build-scripts "${SRC_DIR}"/build/scripts \
+   egg_info --egg-base "${SRC_DIR}"/build
+
+$PYTHON setup.py build \
+   --build-base "${SRC_DIR}"/build \
+   --build-platlib "${SRC_DIR}"/build/lib \
+   --build-purelib "${SRC_DIR}"/build/lib \
+   --build-scripts "${SRC_DIR}"/build/scripts \
+   egg_info --egg-base "${SRC_DIR}"/build \
+   install --compile -O2 --root=/
